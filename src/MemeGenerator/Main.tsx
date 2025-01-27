356424ae6f8519fdc0d11b2450d19684662fd3b9
Pooja Cholera <poojacholera1@gmail.com>
2025-01-27 18:07:38 -0500

    Meme generator project complete | it fetches from the api and stores in an array using useEffect | clears understanding of the useEffect

--- src/MemeGenerator/Main.tsx
+++ src/MemeGenerator/Main.tsx
@@ -1,14 +1,39 @@
 import "./MemeGenerator.css"
 import sampleMemeImg from "../assets/cat-meme.jpeg"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Main(){
 
     const [meme, setMeme] = useState({
-        topText:"One does not simply",
-        bottomText:"Walk into Mordor",
+        topText:"",
+        bottomText:"",
         imgUrl: sampleMemeImg,
     })
+    const handleChange =(event)=>{
+       const {value,name} = event.currentTarget;
+       console.log(value);
+        setMeme(prevMeme=>({
+            ...prevMeme,
+            [name]:value
+        }))
+    }
+    //fetch data from api to get imgUrls data set
+    const [allMemes, setAllMemes] = useState([]);
+    useEffect(()=>{
+        fetch("https://api.imgflip.com/get_memes")
+        .then(res =>res.json())
+        .then(data => setAllMemes(data.data.memes))
+        //console.log(data.data.memes.url);
+    },[]);
+
+    const getMemeImage = () =>{
+        const randomNumber= Math.floor(Math.random() * allMemes.length);
+        const newMemeUrl = allMemes[randomNumber].url;
+        setMeme(prevMeme => ({
+           ...prevMeme,
+        imgUrl:newMemeUrl
+     }))
+    }
 
     return(
         <>
@@ -19,7 +44,8 @@ function Main(){
                         type="text"
                         placeholder="One does not simply"
                         name="topText"
-                     
+                        onChange={handleChange}
+                        value={meme.topText}
                     />
                 </label>
 
@@ -28,10 +54,11 @@ function Main(){
                         type="text"
                         placeholder="Walk into Mordor"
                         name="bottomText"
-                        
+                        onChange={handleChange}
+                        value={meme.bottomText}
                     />
                 </label>
-                <button>Get a new meme image </button>
+                <button onClick={getMemeImage}>Get a new meme image </button>
             </div>
             <div className="meme">
                 <img src={meme.imgUrl} />
--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* {<DescriptionPage/>} */}
     {/* <Traveljournal/> */}
-    {/* {<ChefDevi/>} */}
-    {<MemeGenerator />}
+    {<ChefDevi/>}
+    {/* {<MemeGenerator />} */}
   </StrictMode>,
 )
