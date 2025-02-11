ad351f84e0b99e3bc19ec7f7ae724b36a2e34403
Pooja Cholera <poojacholera1@gmail.com>
2025-02-11 17:30:07 -0500

    env changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 import react from '@vitejs/plugin-react'
 import dotenv from "dotenv";
 
 // https://vite.dev/config/
 dotenv.config();
-export default defineConfig(
-  {
-  plugins: [react()],
-  envDir: './env'
-})
+export default defineConfig(({mode})=>{
+
+  const env = loadEnv(mode,"./env","" )
+  return {
+    plugins: [react()],
+    envDir: './env',
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+   
+    distDir: "dist",
+  }
+}
+ )
